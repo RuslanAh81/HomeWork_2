420899291952a8009022d53743cd35eba708aded
Ruslan <arhara81@gmail.com>
2022-10-06 11:18:27 +0300

    Zala4a 1

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+// напишите программу,которая на вход принимает трехзначное число и на выходе показывает вторую цифру этого числа.
+Console.WriteLine("Введите трехзначное число :");
+int numb = Convert.ToInt32(Console.ReadLine());
+
+int FindNumb2(int num)
+{
+    int des = num % 100;
+    int ed = des / 10;
+    return ed;
+}
+int res = FindNumb2(numb);
+Console.Write( "Вторая цифра " +  res);
+
+*/
