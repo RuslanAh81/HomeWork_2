ed11ff1e3a1263d723acd0973611ab835a3cdf4e
Ruslan <arhara81@gmail.com>
2022-10-10 18:14:27 +0300

    dz2 completed

--- Program.cs
+++ Program.cs
@@ -13,3 +13,59 @@ int res = FindNumb2(numb);
 Console.Write( "Вторая цифра " +  res);
 
 */
+
+// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+
+ Console.WriteLine("Введите Ваше число ");
+int  num = Convert.ToInt32(Console.ReadLine());
+ int ThirdDigit( int a)
+  {
+    int result;
+    if (num <100)
+    
+      result = 0;
+    else
+    {
+    while(num > 1000)
+      
+      num = num /10;
+      result = num % 10;
+    }
+  return result;
+ }
+  int third = ThirdDigit(num);
+if (third > 0)  
+  Console.WriteLine( "третья цифра " + third); 
+
+else 
+  Console.WriteLine( "В вашем числе нет третьей цифры " ); 
+
+  
+
+  // напишите программу, которая принимает на вход цифру, обозначающую день недели, и  проверяет, является ли этот день выходным.
+/*
+  bool FindVihodnoy(int cur)
+  {
+   bool result;
+    if (cur >5)
+    result = true;
+    
+        else 
+   result= false;
+    return result;
+  }
+
+  Console.WriteLine("Введите цифру дня недели : ");
+
+  int cur = Convert.ToInt32(Console.ReadLine());
+
+  bool Findvih = FindVihodnoy(cur);
+
+ if (Findvih)
+
+  Console.WriteLine("Выходной!!");
+
+else
+
+  Console.WriteLine("Рабочий");
+  */
